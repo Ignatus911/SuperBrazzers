53aca6d86e341a98347a3b4c7d43bd7c1842b0c5
Tymac9n <tymac9n@gmail.com>
2020-09-22 21:04:53 +0300

    Some Troubles

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1000f;
+    [SerializeField] float jumpForce = 5f;
+    float xVelocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        ChangeSpriteDirection();
+        Jump();
+    }
+
+    private void Run()
+    {
+        xVelocity = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, 0f);
+        GetComponent<Animator>().SetBool("isRunning", xVelocity != 0);
+    }
+
+    private void ChangeSpriteDirection()
+    {
+        GetComponent<Transform>().localScale = new Vector3
+            (Mathf.Sign(xVelocity), transform.localScale.y);
+    }
+
+    private void Jump()
+    {
+        //if (Input.GetButtonDown("Jump"))
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("press Jump");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, jumpForce * Time.deltaTime);
+        }
+
+    }
+}
