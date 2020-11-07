623d1c16daada99aedf24b28c02f58ab43d10a6a
Tymac9n <tymac9n@gmail.com>
2020-11-07 22:14:34 +0300

    Вывод надписей в UI работает корректно

--- Assets/Scripts/UIPoints/PointsSpawner.cs
+++ Assets/Scripts/UIPoints/PointsSpawner.cs
@@ -7,38 +7,34 @@ public class PointsSpawner : MonoBehaviour
     [SerializeField] private GameObject spawnedText;
     [SerializeField] private RectTransform canvasRectTransform;
     [SerializeField] public Camera mainCamera;
-    private RectTransform rectTransform;
-    private Vector2 uiOffset;
+    private Canvas myCanvas;
 
 
     private void Awake()
 
     {
-        rectTransform = GetComponent<RectTransform>();
-        uiOffset = new Vector2((float)canvasRectTransform.sizeDelta.x / 2f, (float)canvasRectTransform.sizeDelta.y / 2f);
-        Debug.Log("Canvas center = " + uiOffset.x + "  " + uiOffset.y);
+        myCanvas = GetComponent<Canvas>();
     }
 
     public void SpawnPoints(int points, Transform positionInWorldSpace)
     {
-        //
-        Vector2 viewportPosition = mainCamera.WorldToViewportPoint(positionInWorldSpace.position);
-        Debug.Log("ViewportPosition = " + viewportPosition.x + "  " + viewportPosition.y);
-        Vector2 proportionalPosition = new Vector2(viewportPosition.x * canvasRectTransform.sizeDelta.x, viewportPosition.y * canvasRectTransform.sizeDelta.y);
-        Debug.Log("proportionalPosition = " + proportionalPosition.x + "  " + proportionalPosition.y);
-        rectTransform.localPosition = proportionalPosition - uiOffset;
-        Debug.Log("result localposition = " + rectTransform.localPosition.x + "  " + rectTransform.localPosition.y);
-        //
-        spawnedText.GetComponentInChildren<Text>().text = points.ToString();
-        spawnedText.GetComponent<RectTransform>().position = rectTransform.localPosition;
-        var textOnScreen = Instantiate(spawnedText, canvasRectTransform);
-        //textOnScreen.GetComponent<AnimatedText>().SetPosition(rectTransform.localPosition);
-        Debug.Log("result worldposition = " + rectTransform.position.x + "  " + rectTransform.position.y);
-        //public static Object Instantiate(Object original, Vector3 position, Quaternion rotation, Transform parent);
+        var textOnScreen = InstantiateTextOnCanvas(positionInWorldSpace);
+        textOnScreen.GetComponentInChildren<Text>().text = points.ToString();
     }
 
-    public void Spawn1UP()
+    public void Spawn1UP(Transform positionInWorldSpace)
     {
-        GetComponent<Text>().text = "1UP";
+        var textOnScreen = InstantiateTextOnCanvas(positionInWorldSpace);
+        textOnScreen.GetComponentInChildren<Text>().text = "1UP";
+    }
+
+    private GameObject InstantiateTextOnCanvas(Transform positionInWorldSpace)
+    {
+        Vector2 viewportPosition = mainCamera.WorldToViewportPoint(positionInWorldSpace.position);
+        Vector2 proportionalPosition = new Vector2(viewportPosition.x * canvasRectTransform.sizeDelta.x, viewportPosition.y * canvasRectTransform.sizeDelta.y);
+        Vector2 positionOncanvas = (proportionalPosition * myCanvas.scaleFactor);
+        var textOnScreen = Instantiate(spawnedText, canvasRectTransform);
+        textOnScreen.GetComponent<RectTransform>().position = positionOncanvas;
+        return textOnScreen;
     }
 }
